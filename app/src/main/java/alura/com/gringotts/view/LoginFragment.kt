963897369b3222c16a24258e8b98343e98895ad6
Rafael-code-maker <rafaeldepaulamaia@yahoo.com.br>
2021-06-24 16:01:56 -0300

    Várias implementações

--- app/src/main/java/alura/com/gringotts/view/LoginFragment.kt
+++ app/src/main/java/alura/com/gringotts/view/LoginFragment.kt
@@ -15,6 +15,6 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_initial, container, false)
+        return inflater.inflate(R.layout.fragment_login, container, false)
     }
 }
--- app/src/main/java/alura/com/gringotts/view/SplashFragment.kt
+++ app/src/main/java/alura/com/gringotts/view/SplashFragment.kt
@@ -1,4 +1,20 @@
 package alura.com.gringotts.view
 
+import alura.com.gringotts.R
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
 class SplashFragment {
+
+    fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
 }

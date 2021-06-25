84d07b73b9d2abaa6375a39537fe7bc3cc327d91
Rafael-code-maker <rafaeldepaulamaia@yahoo.com.br>
2021-06-25 17:25:20 -0300

    Projeto funcionando, 3 telas vinculadas v2.0

--- app/src/main/java/alura/com/gringotts/view/OnboardingFragment.kt
+++ app/src/main/java/alura/com/gringotts/view/OnboardingFragment.kt
@@ -1,15 +1,24 @@
 package alura.com.gringotts.view
-
 import alura.com.gringotts.R
+import alura.com.gringotts.databinding.FragmentOnboardingBinding
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import registerUrl
 
 class OnboardingFragment : Fragment() {
+
+    private lateinit var binding: FragmentOnboardingBinding
+    private lateinit var criarConta: Button
+    private lateinit var jaTenhoConta: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,7 +26,11 @@ class OnboardingFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_onboarding, container, false)
+        binding = FragmentOnboardingBinding.inflate(layoutInflater) //Setando o nosso Layout de login
+        val view = binding.root //Setando o nosso Layout de login
+
+        criarConta = binding.buttonCriarConta
+        jaTenhoConta = binding.textViewJaTenhoCadastro
 
         fun onBoardingFinished(){ //Salva que a onboard foi executada para não ter que repetir
             val sharedPref = requireActivity().getSharedPreferences("onBoarding", Context.MODE_PRIVATE)
@@ -26,7 +39,15 @@ class OnboardingFragment : Fragment() {
             editor.apply()
         }
 
-        view.setOnClickListener {
+        criarConta.setOnClickListener { //errado
+            @Override
+            fun onClickRegister() {
+                val i = Intent(Intent.ACTION_VIEW)
+                i.data = Uri.parse(registerUrl)
+            }
+        }
+
+        jaTenhoConta.setOnClickListener {
             findNavController().navigate(R.id.action_onboardingFragment_to_loginFragment)
             onBoardingFinished()
         }

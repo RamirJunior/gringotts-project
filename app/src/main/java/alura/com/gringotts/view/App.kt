6aad59b35fa8e130a8ea8c41f2034fe62a76db40
Guilherme Schuffner <guischuffner@gmail.com>
2021-07-01 13:40:55 -0300

    Refatorando App

--- app/src/main/java/alura/com/gringotts/view/App.kt
+++ app/src/main/java/alura/com/gringotts/view/App.kt
@@ -9,7 +9,6 @@ import org.koin.core.context.startKoin
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-
         startKoin {
             androidContext(this@App)
             modules(listOf(initialModule))

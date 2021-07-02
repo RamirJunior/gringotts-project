d785c3ba6f5710a6982fdd9aa2b78408788680af
lucasomaciel <lucasmaciel7l@gmail.com>
2021-07-02 14:59:51 -0300

    Iniciando o ViewPager

---
+++ app/src/main/java/alura/com/gringotts/view/ViewPagerFragment.kt
@@ -0,0 +1,67 @@
+package alura.com.gringotts.view
+
+import alura.com.gringotts.R
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager.widget.ViewPager
+
+
+class ViewPagerFragment : Fragment() {
+    private lateinit var demoCollectionPagerAdapter: DemoCollectionPagerAdapter
+    private lateinit var viewPager: ViewPager
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        demoCollectionPagerAdapter = DemoCollectionPagerAdapter(childFragmentManager)
+        viewPager = view.findViewById(R.id.pager)
+        viewPager.adapter = demoCollectionPagerAdapter
+    }
+}
+
+
+class DemoCollectionPagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
+
+    override fun getCount(): Int  = 3
+
+    override fun getItem(i: Int): Fragment {
+        val fragment = DemoObjectFragment()
+        fragment.arguments = Bundle().apply {
+
+            putInt(ARG_OBJECT, i + 1)
+        }
+        return fragment
+    }
+
+    override fun getPageTitle(position: Int): CharSequence {
+        return "OBJECT ${(position + 1)}"
+    }
+}
+
+private const val ARG_OBJECT = "object"
+
+class DemoObjectFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        arguments?.takeIf { it.containsKey(ARG_OBJECT) }?.apply {
+            val textView: TextView = view.findViewById(android.R.id.text1)
+            textView.text = getInt(ARG_OBJECT).toString()
+        }
+    }
+}

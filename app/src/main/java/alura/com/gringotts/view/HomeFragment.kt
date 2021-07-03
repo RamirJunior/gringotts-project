16bfc6a2978d5e16478e8b985867724c684026bd
Guilherme Schuffner <guischuffner@gmail.com>
2021-07-03 13:46:18 -0300

    recyclerView

--- app/src/main/java/alura/com/gringotts/view/HomeFragment.kt
+++ app/src/main/java/alura/com/gringotts/view/HomeFragment.kt
@@ -1,6 +1,9 @@
 package alura.com.gringotts.view
 
 import alura.com.gringotts.R
+import alura.com.gringotts.data.model.Benefits
+import alura.com.gringotts.databinding.FragmentHomeBinding
+import alura.com.gringotts.databinding.FragmentLoginBinding
 import alura.com.gringotts.presentation.HomeViewModel
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -12,17 +15,22 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 class HomeFragment : Fragment() {
 
     private val homeViewModel by viewModel<HomeViewModel>()
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding= FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val benefitsList = listOf(Benefits("color", "image", "title", "message", "link"),
+            Benefits("color1", "image1", "title1", "message1", "link1"))
         homeViewModel.getHomeData()
     }
 }

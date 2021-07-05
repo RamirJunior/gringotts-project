9e592ede8aaf1ea5ce82a5043790b2edef07cb93
Mikael Araya <arayamikael11@gmail.com>
2021-07-05 14:27:12 -0300

    adição do método getBalance

--- app/src/main/java/alura/com/gringotts/data/repositories/HomeRepository.kt
+++ app/src/main/java/alura/com/gringotts/data/repositories/HomeRepository.kt
@@ -2,7 +2,9 @@ package alura.com.gringotts.data.repositories
 
 import alura.com.gringotts.data.SessionManager
 import alura.com.gringotts.data.api.ApiInterface
+import alura.com.gringotts.data.model.Balance
 import alura.com.gringotts.data.model.HomeResponse
+import android.util.Log
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
@@ -23,6 +25,12 @@ class HomeRepository(private val sessionManager: SessionManager) {
 
     fun saveHomeResponse(homeResponse: HomeResponse) {
         return sessionManager.saveHomeResponse(homeResponse)
+    }
+
+    fun getBalance(): Balance {
+        val response = getHomeResponse()
+        val balance = response!!.balance
+        return balance
     }
 
 }

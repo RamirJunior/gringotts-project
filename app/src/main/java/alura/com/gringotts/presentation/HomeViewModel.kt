dfe85a09c2cfc6138f00ef7ea273c0a654ae7eb8
Guilherme Schuffner <guischuffner@gmail.com>
2021-07-06 16:23:54 -0300

    adicionar botao esconder saldo

--- app/src/main/java/alura/com/gringotts/presentation/HomeViewModel.kt
+++ app/src/main/java/alura/com/gringotts/presentation/HomeViewModel.kt
@@ -17,8 +17,13 @@ class HomeViewModel(private val HomeRepository: HomeRepository) : ViewModel() {
     private val _benefits = MutableLiveData<List<Benefit>>()
     val benefits: LiveData<List<Benefit>> = _benefits
 
-    private val _balance = MutableLiveData<Balance>()
-    val balance: LiveData<Balance> = _balance
+    private lateinit var balanceValue: Balance
+
+    private var hideBalance = false
+    private val _balance = MutableLiveData<String>()
+    val balance: LiveData<String> = _balance
+    private val _receivable = MutableLiveData<String>()
+    val receivable: LiveData<String> = _receivable
 
     init {
         _loading.postValue(true)
@@ -26,8 +31,24 @@ class HomeViewModel(private val HomeRepository: HomeRepository) : ViewModel() {
         viewModelScope.launch {
             val homeApiData = HomeRepository.homeData()
             _benefits.postValue(homeApiData.benefits)
-            _balance.postValue(homeApiData.balance)
+            balanceValue=homeApiData.balance
+            _balance.postValue(balanceValue.currentValue.toString())
+            _receivable.postValue(balanceValue.receivables.toString())
             _loading.postValue(false)
         }
+    }
+    fun hideBalanceButtonClicked() {
+        hideBalance = if(!hideBalance){
+            _balance.postValue(HIDDENVALUE)
+            _receivable.postValue(HIDDENVALUE)
+            true
+        } else{
+            _balance.postValue(balanceValue.currentValue.toString())
+            _receivable.postValue(balanceValue.receivables.toString())
+            false
+        }
+    }
+    companion object{
+        private const val HIDDENVALUE = "- - - -"
     }
 }

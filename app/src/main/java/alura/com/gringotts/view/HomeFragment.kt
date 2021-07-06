eb10f94fa98babfe2097d0a9e73079d152602184
Rafael-code-maker <rafaeldepaulamaia@yahoo.com.br>
2021-07-06 15:50:43 -0300

    Benefits refatorado

--- app/src/main/java/alura/com/gringotts/view/HomeFragment.kt
+++ app/src/main/java/alura/com/gringotts/view/HomeFragment.kt
@@ -41,7 +41,7 @@ class HomeFragment : Fragment() {
 
         homeViewModel.balance.observe(viewLifecycleOwner) {
             binding.balanceValue.text = it.currentValue.toString()
-            //binding. .text = it.receivables.toString()
+            binding.receivableValue.text = it.receivables.toString()
         }
 
         binding.hideBalance.setOnClickListener(){

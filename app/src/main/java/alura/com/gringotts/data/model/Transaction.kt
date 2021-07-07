c3948a98a5b9e4c9bff2090891b72dbe510b0264
Rafael-code-maker <rafaeldepaulamaia@yahoo.com.br>
2021-07-07 16:53:07 -0300

    Mudanças sprint 3

---
+++ app/src/main/java/alura/com/gringotts/data/model/Transaction.kt
@@ -0,0 +1,12 @@
+package alura.com.gringotts.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Transaction(
+    @SerializedName("status") val status: String,
+    @SerializedName("date") val date: String,
+    @SerializedName("time") val time: String,
+    @SerializedName("type") val type: String,
+    @SerializedName("type_description") val typeDescription: String,
+    @SerializedName("value") val value: String
+)

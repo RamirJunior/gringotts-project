bffc5098f637e09f22127dd2ab91bea719f0a920
Guilherme Schuffner <guischuffner@gmail.com>
2021-07-07 17:21:51 -0300

    lidando com datas

--- app/src/main/java/alura/com/gringotts/presentation/ExtractViewModel.kt
+++ app/src/main/java/alura/com/gringotts/presentation/ExtractViewModel.kt
@@ -1,7 +1,27 @@
 package alura.com.gringotts.presentation
 
 import alura.com.gringotts.data.repositories.ExtractRepository
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ExtractViewModel(private val extractRepository: ExtractRepository) : ViewModel() {
+    private val dataAtual: String = ""
+
+    fun getCalendar(){
+        val currentDate = Calendar.getInstance()
+        val sevenDaysAgo = Calendar.getInstance()
+        sevenDaysAgo.timeInMillis = (currentDate.timeInMillis - 7 * MILLIS_DAY)
+        Log.e("aaaa", formatDate(sevenDaysAgo.time))
+    }
+
+    private fun formatDate(date : Date) : String {
+        val formatter = SimpleDateFormat("dd/MM/yyyy", Locale.US)
+        return formatter.format(date)
+    }
+
+    companion object {
+        private const val MILLIS_DAY: Long = 86400000
+    }
 }
--- app/src/main/java/alura/com/gringotts/view/ExtractFragment.kt
+++ app/src/main/java/alura/com/gringotts/view/ExtractFragment.kt
@@ -1,6 +1,32 @@
 package alura.com.gringotts.view
 
+import alura.com.gringotts.databinding.FragmentHomeBinding
+import alura.com.gringotts.presentation.ExtractViewModel
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class ExtractFragment : Fragment() {
+class ExtractFragment: Fragment() {
+    private val extractViewModel by viewModel<ExtractViewModel>()
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?){
+        super.onViewCreated(view, savedInstanceState)
+        extractViewModel.getCalendar()
+        Log.e("aaaa", "aaaaaa")
+    }
 }

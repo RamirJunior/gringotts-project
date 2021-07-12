24d4de67186cd123954278222514778b101c1b6b
Rafael-code-maker <rafaeldepaulamaia@yahoo.com.br>
2021-07-12 17:24:02 -0300

    Filter Fragment carregando o layout do fragmentlistadapter

--- app/src/main/java/alura/com/gringotts/view/home/fragments/FilterFragment.kt
+++ app/src/main/java/alura/com/gringotts/view/home/fragments/FilterFragment.kt
@@ -1,7 +1,7 @@
 package alura.com.gringotts.view.home.fragments
 
 import alura.com.gringotts.data.models.home.Filter
-import alura.com.gringotts.databinding.FragmentAccountStatementBinding
+import alura.com.gringotts.databinding.FilterFragmentBinding
 import alura.com.gringotts.view.home.adapters.FilterListAdapter
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,21 +10,21 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class FilterFragment : Fragment() {
-    private var _binding: FragmentAccountStatementBinding? = null
-    private val binding: FragmentAccountStatementBinding get() = _binding!!
+    private var _binding: FilterFragmentBinding? = null
+    private val binding: FilterFragmentBinding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentAccountStatementBinding.inflate(layoutInflater)
+        _binding = FilterFragmentBinding.inflate(layoutInflater)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.recyclerViewTransactions.adapter =
+        binding.filterRecyclerView.adapter =
             FilterListAdapter(transactionsDaysFilter())
     }
 

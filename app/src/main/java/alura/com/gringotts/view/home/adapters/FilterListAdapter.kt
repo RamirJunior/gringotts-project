576690b9cdcae5dfc4c1c9c4c72eaf5a99ba6981
Rafael-code-maker <rafaeldepaulamaia@yahoo.com.br>
2021-07-16 14:54:41 -0300

    Começando alterações no Filter onBindViewHolder

--- app/src/main/java/alura/com/gringotts/view/home/adapters/FilterListAdapter.kt
+++ app/src/main/java/alura/com/gringotts/view/home/adapters/FilterListAdapter.kt
@@ -1,12 +1,12 @@
 package alura.com.gringotts.view.home.adapters
 
-import alura.com.gringotts.R
 import alura.com.gringotts.data.models.home.Filter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CheckedTextView
 import androidx.recyclerview.widget.RecyclerView
+
 
 class FilterListAdapter(
     private val filter: List<Filter>,
@@ -32,7 +32,18 @@ class FilterListAdapter(
         holder.bind(filter[position])
         holder.itemView.setOnClickListener {
             positionSelect = position
-
+//            val value: Boolean = holder.simpleCheckedTextView.isChecked()
+//            if (value) {
+//                // set check mark drawable and set checked property to false
+//                holder.simpleCheckedTextView.setCheckMarkDrawable(R.drawable.check_ic)
+//                holder.simpleCheckedTextView.setChecked(false)
+//                Toast.makeText(context, "un-Checked", Toast.LENGTH_LONG).show()
+//            } else {
+//                // set check mark drawable and set checked property to true
+//                holder.simpleCheckedTextView.setCheckMarkDrawable(R.drawable.check)
+//                holder.simpleCheckedTextView.setChecked(true)
+//                Toast.makeText(context, "Checked", Toast.LENGTH_LONG).show()
+//            }
             selectItemFilterListener.returnPosition(positionSelect)
         }
     }

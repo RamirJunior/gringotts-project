da05934fad531018c6b5628765afcb7b33c7059d
Rafael-code-maker <rafaeldepaulamaia@yahoo.com.br>
2021-07-20 15:20:41 -0300

    Fix Pix appbar to toolbar

--- app/src/main/java/alura/com/gringotts/view/pix/fragments/PixFragment.kt
+++ app/src/main/java/alura/com/gringotts/view/pix/fragments/PixFragment.kt
@@ -26,7 +26,7 @@ class PixFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btBackPix.setOnClickListener {
+        binding.toolbarPix.setOnClickListener {
             activity?.finish()
         }
 

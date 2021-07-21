05af0bbb59e1f0ef9af367034c4a4d97d53a33a0
Guilherme Schuffner <guischuffner@gmail.com>
2021-07-21 16:34:44 -0300

    pix classe

--- app/src/main/java/alura/com/gringotts/data/models/pix_transference/Pix.kt
+++ app/src/main/java/alura/com/gringotts/data/models/pix_transference/Pix.kt
@@ -1,5 +1,13 @@
 package alura.com.gringotts.data.models.pix_transference
 
-data class Pix (val email : String) {
+import java.util.*
 
+class Pix {
+    lateinit var receiverEmail: String
+    var pixValue: Float = 0.0F
+    lateinit var receiverName: String
+    lateinit var receiverCPF: String
+    lateinit var message: String
+    lateinit var institution: String
+    lateinit var date: Date
 }

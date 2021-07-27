d0b7ebb4abf02a8a3208c888525b000fc25e8dd2
Rafael-code-maker <rafaeldepaulamaia@yahoo.com.br>
2021-07-27 15:00:54 -0300

    API sprint5

--- app/src/main/java/alura/com/gringotts/presentation/pix_transference/ConfirmationPixViewModel.kt
+++ app/src/main/java/alura/com/gringotts/presentation/pix_transference/ConfirmationPixViewModel.kt
@@ -1,6 +1,5 @@
 package alura.com.gringotts.presentation.pix_transference
 
-import alura.com.gringotts.data.repositories.pix_transference.ConfirmationRepository
 import alura.com.gringotts.presentation.pix_transference.auxiliar.SingleLiveEvent
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel

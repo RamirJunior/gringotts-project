de71280972134309b19a651c91ccc8c10965fbfc
Guilherme Schuffner <guischuffner@gmail.com>
2021-08-03 18:34:20 -0300

    header interceptor

---
+++ app/src/main/java/alura/com/gringotts/data/interceptors/HeaderInterceptor.kt
@@ -0,0 +1,11 @@
+package alura.com.gringotts.data.interceptors
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class HeaderInterceptor: Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        TODO("Not yet implemented")
+    }
+
+}

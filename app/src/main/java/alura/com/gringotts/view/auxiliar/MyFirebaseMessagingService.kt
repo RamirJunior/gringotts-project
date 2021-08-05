4dfe240cac8ff3ae07ff13687edbfb7375eb6f77
Ramir Junior <junior.ribeiro0@live.com>
2021-08-05 16:37:18 -0300

    implementation foreground notification

---
+++ app/src/main/java/alura/com/gringotts/view/auxiliar/MyFirebaseMessagingService.kt
@@ -0,0 +1,66 @@
+package alura.com.gringotts.view.auxiliar
+
+import alura.com.gringotts.R
+import alura.com.gringotts.view.initial.InitialActivity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import kotlin.random.Random
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+        val title = remoteMessage.notification?.title
+        val body = remoteMessage.notification?.body
+        notificatioReceived(applicationContext, title, body)
+    }
+
+    override fun onNewToken(remoteMessage: String) {
+        super.onNewToken(remoteMessage)
+    }
+
+    fun notificatioReceived(context: Context, title: String?, message: String?) {
+
+        val notificationChannel: NotificationChannel
+        val builder: NotificationCompat.Builder
+        val channelId = "alura.com.gringotts"
+        val notificationManager: NotificationManager = applicationContext
+            .getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val id = Random.nextInt(1, 100)
+        val intent = Intent(this, InitialActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationChannel =
+                NotificationChannel(channelId, message, NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(notificationChannel)
+
+            builder = NotificationCompat.Builder(this, channelId)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setAutoCancel(true)
+                .setSmallIcon(R.mipmap.ic_launcher_foreground)
+                .setContentTitle(title)
+                .setStyle(NotificationCompat.BigTextStyle()
+                    .bigText(message)
+                    .setSummaryText("Notificação"))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+        } else {
+            builder = NotificationCompat.Builder(this)
+                .setAutoCancel(true)
+                .setSmallIcon(R.mipmap.ic_launcher_foreground)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+        }
+        notificationManager.notify(id, builder.build())
+    }
+
+}
